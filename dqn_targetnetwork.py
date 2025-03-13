34e3af7a4b0f53371d101c07687bc8ab70567215
jaezr1 <mochamadrezaarimurti@gmail.com>
2025-03-13 10:18:47 +0700

    Update dqn_targetnetwork.py

--- dqn_targetnetwork.py
+++ dqn_targetnetwork.py
@@ -62,13 +62,13 @@ if __name__ == '__main__':
     state_size = env.observation_space.shape[0]
     action_size = env.action_space.n
     agent = DQNAgent(state_size, action_size)
-    episodes = 50
+    episodes = 1000
     batch_size = 32
 
     for e in range(episodes):
         state, _ = env.reset()
         state = np.reshape(state, [1, state_size])
-        for time in range(500):
+        for time in range(1000):
             action = agent.act(state)
             next_state, reward, done, _, _ = env.step(action)
             reward = reward if not done else -10
